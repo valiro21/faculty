a570d76b64579058999f9dc08e0a297ce2d92582
Valentin Rosca <vrosca@192-168-0-104.rdsnet.ro>
2016-10-07 12:56:07 +0300

    Faculty first commit

---
+++ ag/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "hill_climbing.h"
+
+using namespace std;
+
+double function (double *vals) {
+	return 3. * vals[0]*vals[0] - 5. * vals[1] * vals[2];
+}
+
+double comp (double a, double b) {
+	return a < b ? true : false;
+}
+
+int main () {
+	double start[3];
+	start[1] = 200;
+	start[2] = 211;
+	start[3] = -222;
+	double *result = hillClimbing (3, -1, 0.2, start, function, comp);
+	for (int i = 0; i < 3; i++)
+		cout << result[i] << ' ';
+	return 0;
+}
