93bf398f8bbd2e91fd74debea6c8e7428924edce
Valentin Rosca <vrosca@192-168-0-104.rdsnet.ro>
2016-10-13 15:48:13 +0300

    Retele failed myfind with form

--- an2/ag/main.cpp
+++ an2/ag/main.cpp
@@ -11,12 +11,17 @@ double comp (double a, double b) {
 	return a < b ? true : false;
 }
 
+int start (int n, ...) {
+	arg
+}
+
 int main () {
 	double start[3];
 	start[1] = 200;
 	start[2] = 211;
 	start[3] = -222;
 	double *result = hillClimbing (3, -1, 0.2, start, function, comp);
+	result = hillClimbing (3, -1, 0.2, start, function2, comp);
 	for (int i = 0; i < 3; i++)
 		cout << result[i] << ' ';
 	return 0;
